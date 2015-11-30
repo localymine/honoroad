fdb19ee25d094b8ffc801049f3ea4da79cfbd9d9
Kantoh <leduongkhang@gmail.com>
2015-11-30 17:06:40 +0700

    update acf

--- public_html/wp-content/themes/honoroad2015/includes/cpt_acf_definitions.php
+++ public_html/wp-content/themes/honoroad2015/includes/cpt_acf_definitions.php
@@ -56,7 +56,7 @@ function cptui_register_my_cpts() {
         "exclude_from_search" => false,
         "capability_type" => "post",
         "map_meta_cap" => true,
-        "hierarchical" => true,
+        "hierarchical" => false,
         "rewrite" => array("slug" => "product", "with_front" => true),
         "query_var" => true,
         "menu_position" => 26,
